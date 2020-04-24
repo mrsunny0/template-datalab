753a0897036fcf81c3580c11a381e3ae1c8c78ca
George Sun <george.lele.sun@gmail.com>
2020-04-23 23:29:54 -0400

    initial structure

---
+++ src/analyze/main.py
@@ -0,0 +1,7 @@
+import numpy as np
+import scipy as sp
+import pandas as pd
+from matplotlib import pyplot as plt
+import pickle
+import sklearn
+import boto3
---
+++ src/download/main.py
@@ -0,0 +1,4 @@
+import numpy as np
+import pandas as pd
+import boto3
+from matplotlib import pyplot as plt
